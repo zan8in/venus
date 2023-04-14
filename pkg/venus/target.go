38b62a1058ae31bb341cafff45eb3429db3264e1
zanbin168 <zanbin168@qq.com>
2023-04-14 15:42:59 +0800

    update

--- pkg/venus/target.go
+++ pkg/venus/target.go
@@ -1,54 +1,25 @@
 package venus
 
 import (
-	"bufio"
-	"io"
-	"os"
-
-	"github.com/remeh/sizedwaitgroup"
+	fileutil "github.com/zan8in/pins/file"
 )
-
-var TargetTempFile = "venus-target-temp-*"
 
 func (r *Runner) PreprocessTarget() error {
 	var (
 		err error
 	)
 
-	targetTemp, err := os.CreateTemp("", TargetTempFile)
-	if err != nil {
-		return err
-	}
-	defer targetTemp.Close()
-
 	if len(r.options.TargetFile) > 0 {
-		f, err := os.Open(r.options.TargetFile)
+
+		flist, err := fileutil.ReadFile(r.options.TargetFile)
 		if err != nil {
 			return err
 		}
-		defer f.Close()
 
-		if _, err := io.Copy(targetTemp, f); err != nil {
-			return err
+		for f := range flist {
+			r.options.Target.Set(f)
 		}
 	}
-
-	f, err := os.Open(targetTemp.Name())
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	wg := sizedwaitgroup.New(100)
-	s := bufio.NewScanner(f)
-	for s.Scan() {
-		wg.Add()
-		go func(target string) {
-			defer wg.Done()
-			r.options.Target.Set(target)
-		}(s.Text())
-	}
-	wg.Wait()
 
 	return err
 }
